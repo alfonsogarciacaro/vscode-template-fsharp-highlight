0a9d37452d32beba159efe80f959b8df0418f65e
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2021-09-08 14:49:16 +0900

    Add autocomplete support for html/css templates

---
+++ src/extension.ts
@@ -0,0 +1,206 @@
+import { commands, CompletionList, ExtensionContext, Hover, languages, SnippetString, Uri, workspace } from 'vscode';
+
+function last<T>(xs: Iterable<T>): T | undefined {
+	let res: T | undefined;
+	for (let x of xs) {
+		res = x;
+	}
+	return res;
+}
+
+function mapToArray<T, T2>(xs: Iterable<T>, f: (x: T) => T2): T2[] {
+	const results = [];
+	for (let x of xs) {
+		results.push(f(x))
+	}
+	return results;
+}
+
+function tryFirst<T>(...ops: (() => T)[]): T | undefined {
+	for (let op of ops) {
+		const result = op();
+		if (result != null) {
+			return result;
+		}
+	}
+}
+
+function findNonNestedBraces(text: string): { start: number, end: number }[] {
+	const results = [];
+	const braces =
+		[
+			mapToArray(text.matchAll(/(?<!\{)\{(?!\{)/g), m => ({ index: m.index, isOpen: true })),
+			mapToArray(text.matchAll(/(?<!\})\}(?!\})/g), m => ({ index: m.index + 1, isOpen: false })),
+		]
+			.flat()
+			.sort((x, y) => x.index > y.index ? 1 : x.index == y.index ? 0 : -1);
+
+	let nestedLevel = 0;
+	let lastOpenBrace: number | undefined;
+	for (let brace of braces) {
+		if (brace.isOpen) {
+			if (lastOpenBrace == null) {
+				lastOpenBrace = brace.index
+			} else {
+				nestedLevel++;
+			}
+		} else {
+			if (nestedLevel === 0) {
+				if (lastOpenBrace != null) {
+					results.push({ start: lastOpenBrace, end: brace.index });
+					lastOpenBrace = null;
+				}
+			} else {
+				nestedLevel--;
+			}
+		}
+	}
+
+	return results;
+}
+
+function replaceWithWhitespace(text: string) {
+	return text
+		.split('\n')
+		.map(line => {
+			return ' '.repeat(line.length);
+		}).join('\n');
+}
+
+function replaceNonNestedBracesWithWhitespace(text: string) {
+	const braces = findNonNestedBraces(text);
+	if (braces.length === 0) {
+		return text;
+	} else {
+		let newText = "";
+		let lastIndex = 0;
+		for (let brace of braces) {
+			newText += text.slice(lastIndex, brace.start);
+			newText += replaceWithWhitespace(text.slice(brace.start, brace.end));
+			lastIndex = brace.end;
+		}
+		newText += text.slice(lastIndex);
+		return newText;
+	}
+}
+
+function tryCssVirtualContent(documentText: string, offset: number) {
+	const virtualContent = tryVirtualContent(documentText, offset, /[ ._]css\s+\$"""/g, /"""/);
+	if (virtualContent != null) return ["css", virtualContent];
+}
+
+function tryHtmlVirtualContent(documentText: string, offset: number) {
+	const virtualContent = tryVirtualContent(documentText, offset, /[ ._](html|svg)\s+\$"""/g, /"""/);
+	if (virtualContent != null) return ["html", virtualContent];
+}
+
+/** startPattern must be a global RegExp */
+function tryVirtualContent(documentText: string, offset: number, startPattern: RegExp, endPattern: RegExp): string | undefined {
+	let startMatches = Array.from(documentText.slice(0, offset).matchAll(startPattern));
+	if (startMatches.length === 0) {
+		return;
+	}
+
+	const lastStartMatch = startMatches[startMatches.length - 1];
+	const regionStart = lastStartMatch.index + lastStartMatch[0].length;
+	if (regionStart === offset) {
+		return;
+	}
+
+	const endMatchBeforeOffset = documentText.slice(regionStart, offset).match(endPattern);
+	if (endMatchBeforeOffset != null) {
+		return;
+	}
+
+	// First fill virtualContent with whitespace
+	let virtualContent = replaceWithWhitespace(documentText);
+
+	for (let startMatch of startMatches) {
+		const regionStart = startMatch.index + startMatch[0].length;
+		const endMatch = documentText.slice(regionStart).match(endPattern);
+		const regionEnd = endMatch ? regionStart + endMatch.index : documentText.length;
+		// Replace F# escape braces
+		let regionContent = documentText.slice(regionStart, regionEnd)
+		regionContent =
+			// Looks like replacing F# content in braces with whitespace doesn't help the autocomplete for html/css
+			// replaceNonNestedBracesWithWhitespace(regionContent)
+			regionContent
+				.replace(/\{\{/g, "{ ")
+				.replace(/\}\}/g, "} ")
+				.replace(/%%/g, "% ");
+		virtualContent = virtualContent.slice(0, regionStart) + regionContent + virtualContent.slice(regionEnd);
+	}
+
+	return virtualContent;
+}
+
+export function activate(context: ExtensionContext) {
+	const virtualDocumentContents = new Map<string, string>();
+
+	workspace.registerTextDocumentContentProvider('embedded-content', {
+		provideTextDocumentContent: uri => {
+			const originalUri = uri.path.slice(1).slice(0, uri.path.lastIndexOf(".") - 1);
+			// const decodedUri = decodeURIComponent(originalUri);
+			const content = virtualDocumentContents.get(originalUri);
+			return content;
+		}
+	});
+
+	languages.registerCompletionItemProvider(
+		[{ scheme: 'file', language: 'fsharp' }],
+		{
+			async provideCompletionItems(document, position, token, context) {
+				context.triggerKind
+				const documentText = document.getText();
+				const documentOffset = document.offsetAt(position);
+				const virtual = tryFirst(
+					() => tryHtmlVirtualContent(documentText, documentOffset),
+					() => tryCssVirtualContent(documentText, documentOffset),
+				);
+				if (virtual != null) {
+					const [virtualId, virtualContent] = virtual;
+					const originalUri = document.uri.toString();
+					const vdocUriString = `embedded-content://${virtualId}/${encodeURIComponent(
+						originalUri
+						)}.${virtualId}`;
+
+					virtualDocumentContents.set(originalUri, virtualContent);
+					const vdocUri = Uri.parse(vdocUriString);
+
+					// Auto-closing tags is not working so try to provide our own
+					if (context.triggerCharacter === ">") {
+						if ((virtualId === "html" || virtualId === "svg")) {
+							if (documentText[documentOffset - 2] !== "/") {
+								const tagMatch = last(virtualContent.slice(0, documentOffset).matchAll(/<([\w-]+)/g));
+								if (tagMatch) {
+									const tag = tagMatch[1];
+									// TODO: complete this list
+									const noClosingTags = ["area", "br", "col", "hr", "img", "input", "link"];
+									if (noClosingTags.indexOf(tag) === -1) {
+										return new CompletionList([{
+											label: `</${tag}>`,
+											insertText: new SnippetString(`$0</${tag}>`,)
+										}])
+									}
+								}
+							}
+						}
+						return;
+					}
+
+					const result = await commands.executeCommand<CompletionList>(
+						'vscode.executeCompletionItemProvider',
+						vdocUri,
+						position,
+						context.triggerCharacter
+					);
+					return result;
+				}
+			}
+		},
+		">"
+	)
+
+	// TODO: Forwarding hover is not working well, not sure why
+	// languages.registerHoverProvider
+}
